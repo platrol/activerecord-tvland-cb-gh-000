182957084d30aa05d3c8dbaa1e15f4dcfe7af00c
norman lewis <takialog@gmail.com>
2018-03-07 14:00:33 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+  def build_show(data)
+    show = Show.find_or_create_by(data)
+    show.characters << self
+    show
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+
+
+    def build_network(data)
+      network = Network.find_or_create_by(data)
+      network.shows << self
+    end
 end
